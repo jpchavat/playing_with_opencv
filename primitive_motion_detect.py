3ad8f1affa3035b4e640363752a964f047d1e233
Jp <jupach@gmail.com>
2015-05-01 00:13:56 -0300

    Improvements using (in a right way) numpy

--- primitive_motion_detect.py
+++ primitive_motion_detect.py
@@ -8,43 +8,35 @@ import time
 
 def doTheThing():
 
+    # Instance of VideoCapture to capture webcam(0) images
     cap = cv2.VideoCapture(0)
     cap.open(0)
+
+    # Getting width and height of captured images
     w = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
     h = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
-    threshold = 25
+    print "Width: ", w, "Height: ", h
 
-    print w, h
+    # Threshold to consider a difference as a difference (u know what I mean)
+    _threshold = 100
+    print "Threshold: ", _threshold
 
     ret, last = cap.read()
+    last = cv2.cvtColor(last, cv2.cv.CV_BGR2GRAY)
     while cap.isOpened():
         ret, frame = cap.read()
-        toShow = np.zeros((h,w), dtype=np.uint8)
+        frame = cv2.cvtColor(frame, cv2.cv.CV_BGR2GRAY)
         if ret is True:
-            for i in range(h):
-                for j in range(w):
-                    p0 = last.item(i, j, 0)
-                    p00 = frame.item(i, j, 0)
+            to_show = np.array(np.where(abs(last - frame) > _threshold, 255, 0),
+                               dtype=np.uint8)
 
-                    if p0 - threshold > p00 or p00 > p0 + threshold:
-                        p1 = last.item(i, j, 1)
-                        p11 = frame.item(i, j, 1)
-                        if p1 - threshold > p11 or p11 > p1 + threshold:
-                            p2 = last.item(i, j, 2)
-                            p22 = frame.item(i, j, 2)
-                            if p2 - threshold > p22 or p22 > p2 + threshold:
-                                toShow[i, j] = 255
+            last = frame.copy()
 
-            cv2.imshow('frame', toShow)
-
-            last = frame
+        cv2.imshow('frame', to_show)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-        time.sleep(0.1)
-
 
 if __name__ == '__main__':
     doTheThing()
-
