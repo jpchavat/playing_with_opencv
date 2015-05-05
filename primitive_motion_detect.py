e41e60e48753011938f040226d07145375c8fa41
Jp <jupach@gmail.com>
2015-05-04 22:33:29 -0300

    Added 2 samples to detect background and weighing on thems. Added text with FPS. Added a variable to skip taking times of taking a new sample

--- primitive_motion_detect.py
+++ primitive_motion_detect.py
@@ -1,4 +1,4 @@
-__author__ = 'jp'
+__author__ = 'jpchavat'
 
 import numpy as np
 import cv2
@@ -18,19 +18,39 @@ def doTheThing():
     print "Width: ", w, "Height: ", h
 
     # Threshold to consider a difference as a difference (u know what I mean)
-    _threshold = 100
+    _threshold = 25
     print "Threshold: ", _threshold
 
+    # Times to skip taking a new background sample
+    times_skip_sample = 5
+    times = times_skip_sample
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
     ret, last = cap.read()
-    last = cv2.cvtColor(last, cv2.cv.CV_BGR2GRAY)
+    last1 = cv2.cvtColor(last, cv2.cv.CV_BGR2GRAY)
+    last2 = last1.copy()
+
+    _time = time.time()
+
     while cap.isOpened():
         ret, frame = cap.read()
         frame = cv2.cvtColor(frame, cv2.cv.CV_BGR2GRAY)
         if ret is True:
-            to_show = np.array(np.where(abs(last - frame) > _threshold, 255, 0),
-                               dtype=np.uint8)
+            to_show = np.array(np.where(abs(last1 - frame) > _threshold,
+                                        255, 0), dtype=np.uint8)
 
-            last = frame.copy()
+        if times == times_skip_sample:
+            last1 = (last2 * 0.1 + last1 * 0.2 + frame * 0.7) #average
+            last2 = last1.copy()
+            timeAux = time.time()
+            _fps = "%.2f" % (times/(timeAux-_time))
+            _time = timeAux
+            times = 0
+        else:
+            times += 1
+
+        cv2.putText(to_show,'FPS: '+_fps,(40,40), font, 1,(255,255,0),2)
 
         cv2.imshow('frame', to_show)
 
